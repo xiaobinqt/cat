b8b541e5c8b9d862cbbbf704ed90277fca6db754
weibin <xiaobinqt@163.com>
2023-09-05 14:43:44 +0800

    pref: lru

--- lru/lru.go
+++ lru/lru.go
@@ -29,3 +29,52 @@ type entry struct {
 type Value interface {
 	Len() int
 }
+
+func (c *Cache) Get(key string) (value Value, ok bool) {
+	if ele, ok := c.cache[key]; ok {
+		c.ll.MoveToBack(ele)
+		kv := ele.Value.(*entry)
+		return kv.value, true
+	}
+	return nil, false
+}
+
+func (c *Cache) RemoveOldest() {
+	ele := c.ll.Front()
+	if ele == nil {
+		return
+	}
+
+	c.ll.Remove(ele)
+	kv := ele.Value.(*entry)
+	delete(c.cache, kv.key)
+	c.nBytes -= int64(len(kv.key)) + int64(kv.value.Len())
+	if c.OnEvicted != nil {
+		c.OnEvicted(kv.key, kv.value)
+	}
+}
+
+func (c *Cache) Add(key string, value Value) {
+	if ele, ok := c.cache[key]; ok {
+		c.ll.MoveToBack(ele) // if exist remove element to back
+		kv := ele.Value.(*entry)
+		c.nBytes += int64(value.Len()) - int64(kv.value.Len())
+		kv.value = value
+	} else {
+		ele := c.ll.PushFront(&entry{
+			key:   key,
+			value: value,
+		})
+		c.cache[key] = ele
+		c.nBytes += int64(len(key)) + int64(value.Len())
+	}
+
+	// judging capacity is full
+	for c.maxBytes != 0 && c.maxBytes < c.nBytes {
+		c.RemoveOldest()
+	}
+}
+
+func (c *Cache) Len() int {
+	return c.ll.Len()
+}
