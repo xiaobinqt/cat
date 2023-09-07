116c06febd2cc1c25e8a8894081521759848c153
weibin <xiaobinqt@163.com>
2023-09-07 15:59:09 +0800

    pref: readthedocs

--- source/conf.py
+++ source/conf.py
@@ -19,15 +19,13 @@ extensions = [
     'recommonmark',
 ]
 
-templates_path = ['_templates']
+# templates_path = ['_templates']
 exclude_patterns = []
 
-language = 'en'
+
 
 # -- Options for HTML output -------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
 
 html_theme = 'sphinx_rtd_theme'
-html_static_path = ['_static']
-
-html_theme_path = [sphinx_rtd_theme.get_html_theme_path()]
+# html_static_path = ['_static']
--- source/index.rst
+++ source/index.rst
@@ -1,5 +1,5 @@
 .. cat documentation master file, created by
-   sphinx-quickstart on Thu Sep  7 10:01:51 2023.
+   sphinx-quickstart on Thu Sep  7 15:56:20 2023.
    You can adapt this file completely to your liking, but it should at least
    contain the root `toctree` directive.
 
